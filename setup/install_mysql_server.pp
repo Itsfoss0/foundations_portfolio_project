b5310ee914ccf4cf6efdfc9f1506b6b1f2890d26
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-22 15:34:19 +0300

    [Chore]: update puppet manifest

--- setup/install_mysql_server.pp
+++ setup/install_mysql_server.pp
@@ -8,6 +8,15 @@ package { 'mysql-client':
   ensure => installed,
 }
 
+package {'python3:
+  ensure => installed,
+}
+
+package {'python3-pip':
+  ensure => installed,
+  require => Package['python3'],
+}
+
 service { 'mysql':
   ensure  => running,
   enable  => true,
@@ -20,4 +29,10 @@ exec { 'mysql_secure_installation':
   creates     => '/etc/mysql/my.cnf',
   refreshonly => true,
   require     => Package['mysql-server'],
+}
+
+exec { 'install_python_packages':
+  command     => 'pip install -r ./requirements.txt',
+  path        => '/usr/local/bin:/usr/bin:/bin',
+  refreshonly => true,
 }
