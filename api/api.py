978c89dc87fec7510f1ab9e6ca08c3d568a259ff
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-23 17:53:06 +0300

    [Lint]: Fix flake8 linter

--- api/api.py
+++ api/api.py
@@ -8,6 +8,7 @@ from bcrypt import gensalt, hashpw
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from flask_restful import Api, Resource
 from MySQLdb import connect, cursors
 from os import getenv
 
@@ -20,40 +21,49 @@ DB_USER = getenv('DB_USER')
 DB_PASSWD = getenv('DB_PASSWD')
 DB_NAME = getenv('DB_NAME')
 
-api = Flask(__name__)
-
-CORS(api)
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
 
 database_object = connect(
-    host = DB_HOST,
-    user = DB_USER,
-    passwd = DB_PASSWD,
-    db = DB_NAME
+    host=DB_HOST,
+    user=DB_USER,
+    passwd=DB_PASSWD,
+    db=DB_NAME
 )
-
 cursor = database_object.cursor(cursorclass=cursors.DictCursor)
 
 
-@api.route("/signup", methods=["POST", "PUT"])
-@cross_origin()
-@validate_user
-def signup():
-    """Handle user signup"""
-    name = request.form.get("name")
-    email = request.form.get('email')
-    password = request.form.get('password')
-    age = request.form.get("age")
-    hashed_pw = hashpw(password.encode('utf-8'), gensalt())
-    insert_values = (name, email, age, hashed_pw)
+class SignUpResource(Resource):
+    """
+    Class to handle user signup
+    """
+    @cross_origin()
+    @validate_user
+    def post(self):
+        """Handle user signup"""
+        name = request.json.get("name")
+        email = request.json.get('email')
+        password = request.json.get('password')
+        age = request.json.get("age")
+        hashed_pw = hashpw(password.encode('utf-8'), gensalt())
+        insert_values = (name, email, age, hashed_pw)
 
-    try:
-        cursor.execute("INSERT INTO `app_users` (name, email, age, password) VALUES (%s, %s, %s, %s)", insert_values)
-        database_object.commit()
-        return jsonify({ "status": "success", "message": f"User {name} created."}), 200
-    except Exception as e:
-        error_message = str(e.__cause__)
-        print(error_message)
-        return jsonify({"error": error_message}), 500
-    
+        try:
+            cursor.execute("INSERT INTO `app_users`\
+                            (name, email, age, password) VALUES\
+                                (%s, %s, %s, %s)", insert_values)
+            database_object.commit()
+            return jsonify({"status": "success",
+                           "message": f"User {name} created."}), 200
+        except Exception as e:
+            error_message = str(e.__cause__)
+            print(error_message)
+            return jsonify({"error": error_message}), 500
+
+
+api.add_resource(SignUpResource, "/signup")
+
+
 if __name__ == "__main__":
-    api.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
