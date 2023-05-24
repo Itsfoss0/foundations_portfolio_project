aaca180c7450a5036cb66d58c5423f8c0ca05145
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-24 19:16:31 +0300

    [Chore]: Add /login endpoint

--- api/app.py
+++ api/app.py
@@ -7,13 +7,14 @@ An API for the project
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
-from auth.users import SignUpResource
+from auth.users import SignUpResource, LoginResource
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
 api.add_resource(SignUpResource, "/signup")
+api.add_resource(LoginResource, "/login")
 
 
 if __name__ == "__main__":
