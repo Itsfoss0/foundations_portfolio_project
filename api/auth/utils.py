14b0d9c63ed7dfaf5aa6c186269a92aeadc8cba9
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-28 19:43:51 +0300

    [Chore]: Add utility functions

--- api/auth/utils.py
+++ api/auth/utils.py
@@ -44,20 +44,23 @@ def get_user_id_from_email(email: str) -> int:
 
 
 def get_all_user_tasks(user_id: int) -> tuple:
-    """
-    Gets all the tasks that belong
-    to the user, using the user_id as
-    the reference point
-    Args:
-        user_id (int): The ID of the user
-    Returns:
-        A list of user's tasks (both done and undone)
-    """
-    cursor_object.execute("SELECT id, title, description,\
-        due_date, updated_at, done FROM tasks WHERE created_by=%s",
-                          (user_id,))
-    tasks_list = cursor_object.fetchall()
-    return tasks_list
+    try:
+        """
+        Gets all the tasks that belong
+        to the user, using the user_id as
+        the reference point
+        Args:
+            user_id (int): The ID of the user
+        Returns:
+            A list of user's tasks (both done and undone)
+        """
+        cursor_object.execute("SELECT id, title, description,\
+            due_date, updated_at, done FROM tasks WHERE created_by=%s",
+                            (user_id,))
+        tasks_list = cursor_object.fetchall()
+        return tasks_list
+    except Exception as e:
+        return (e)
 
 
 def decode_user_tokken(tokken: bytes) -> dict:
@@ -105,3 +108,47 @@ def add_more_tasks(
         return f"New task {task_title} created."
     except Exception as e:
         return f"could not add {task_title}, {str(e)}"
+
+def get_one_task(task_id: int):
+    """
+    Get a task from the database
+    Args:
+        task_id (int): the id of the task
+    Returns:
+        a dictionary of the tasks' props
+    """
+    cursor_object.execute("SELECT * FROM tasks WHERE id = %s", (task_id,))
+    return cursor_object.fetchall()
+    
+
+
+def delete_task(task_id: int, owner_id: int) -> str:
+    """
+    utility function to delete a task
+    from the database
+    Args:
+        task_id (int): The task Id to delete
+        owner_id (int): the `created_by` field
+    # Added the owner_id to make sure one doesn't
+    # Delete tasks that dont belong to them.
+    Returns:
+        str -> Was the operation sucessful
+    """
+    try:
+        cursor_object.execute(
+            "DELETE FROM tasks WHERE created_by = %s\
+                AND id = %s", (owner_id, task_id)
+        )
+        # task_title = get_one_task(task_id)[0].get('title')
+        # total hours wasted here = 1 hour and 13  minutes
+        # Why cant this thing just return the title
+        # print(task_title)
+        db_object.commit()
+        return f"Deleted task {task_id}"
+    except Exception as e:
+        # task doesnt exists,
+        # the User trying to delete a task
+        # is not the owner, or any other exception
+        print(e)
+        return f"Sorry, could not delete task {task_id}"
+
