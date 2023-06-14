f859a6e3d9e3292d7d7660d8df7aeece02fca098
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-14 19:35:06 +0300

    [Chore]: Add Signup script

--- frontend/src/js/signup.js
+++ frontend/src/js/signup.js
@@ -1,56 +1,56 @@
 $(document).ready(function () {
-    function showFlashMessage(type, content) {
-        const flashMessagesContainer = $('#flash-messages');
-        const flashMessage = $(`<div class="${type}">${content}</div>`);
-        flashMessagesContainer.html(flashMessage);
+  function showFlashMessage (type, content) {
+    const flashMessagesContainer = $('#flash-messages');
+    const flashMessage = $(`<div class="${type}">${content}</div>`);
+    flashMessagesContainer.html(flashMessage);
 
-        // Automatically hide the flash message after 3 seconds
-        setTimeout(function () {
-            flashMessage.fadeOut('slow', function () {
-                flashMessage.remove();
-            });
-        }, 3000);
-    }
+    // Automatically hide the flash message after 3 seconds
+    setTimeout(function () {
+      flashMessage.fadeOut('slow', function () {
+        flashMessage.remove();
+      });
+    }, 3000);
+  }
 
-    function sendSignUpData(data) {
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:5000/signup",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            data: JSON.stringify(data),
-            success: function (response) {
-                console.log(response);
-                showFlashMessage('success', response.message)
-            },
-            error: function (xhr, textStatus, errorThrown) {
-                if (xhr.status === 418) {
-                    // console.log("The User already exists.");
-                    showFlashMessage('error', `User with that email already exists`)
-                } else if (xhr.status === 500) {
-                    showFlashMessage('error', 'Internal Server Error, try again')
-                } else {
-                    showFlashMessage('error', 'An error occurred, try again later')
-                }
-            }
-        });
-    }
-
-    $('#signup-form').submit(function (e) {
-        e.preventDefault();
-
-        const email = $('#email').val();
-        const name = $('#name').val();
-        const password = $('#password').val();
-
-        const data = {
-            "email": email,
-            "name": name,
-            "password": password
-        };
-
-        console.log(JSON.stringify(data));
-        sendSignUpData(data);
+  function sendSignUpData (data) {
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:5000/signup',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify(data),
+      success: function (response) {
+        console.log(response);
+        showFlashMessage('success', response.message);
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        if (xhr.status === 418) {
+          // console.log("The User already exists.");
+          showFlashMessage('error', 'User with that email already exists');
+        } else if (xhr.status === 500) {
+          showFlashMessage('error', 'Internal Server Error, try again');
+        } else {
+          showFlashMessage('error', 'An error occurred, try again later');
+        }
+      }
     });
+  }
+
+  $('#signup-form').submit(function (e) {
+    e.preventDefault();
+
+    const email = $('#email').val();
+    const name = $('#name').val();
+    const password = $('#password').val();
+
+    const data = {
+      email: email,
+      name: name,
+      password: password
+    };
+
+    console.log(JSON.stringify(data));
+    sendSignUpData(data);
+  });
 });
